c6ca4acbaabdee3bb498e2513bfd3492c3ef363c
Malgorzata Adamczyk <madamczyk1@student.uw.edu.pl>
2019-03-20 11:16:39 +0100

    #5: Add tests for tf_keras.

--- tests/utils/test_tf_keras.py
+++ tests/utils/test_tf_keras.py
@@ -1,5 +1,21 @@
+import logging
 import mock
-from imageatm.utils.tf_keras import use_multiprocessing
+import os
+import numpy as np
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.utils import np_utils
+from keras.utils.test_utils import get_test_data
+from imageatm.utils.tf_keras import use_multiprocessing, LoggingModels
+
+input_dim = 2
+num_hidden = 4
+num_classes = 2
+batch_size = 5
+train_samples = 20
+test_samples = 20
+
+TEST_DIR = os.path.abspath('tests/data/test_callbacks/')
 
 
 class TestTfKeras(object):
@@ -17,3 +33,186 @@ class TestTfKeras(object):
 
         assert use_multi == True
         assert num_worker == 4711
+
+    def test_LoggingModels(self, mocker):
+        mp_logger_warning = mocker.patch('logging.Logger.warning')
+
+        def get_data_callbacks(
+            num_train=train_samples,
+            num_test=test_samples,
+            input_shape=(input_dim,),
+            classification=True,
+            num_classes=num_classes,
+        ):
+            return get_test_data(
+                num_train=num_train,
+                num_test=num_test,
+                input_shape=input_shape,
+                classification=classification,
+                num_classes=num_classes,
+            )
+
+        tmpdir = TEST_DIR
+        np.random.seed(1337)
+        filepath = str(tmpdir + 'checkpoint.h5')
+        (X_train, y_train), (X_test, y_test) = get_data_callbacks()
+        y_test = np_utils.to_categorical(y_test)
+        y_train = np_utils.to_categorical(y_train)
+        # case 1
+        monitor = 'val_loss'
+        save_best_only = False
+        mode = 'auto'
+
+        model = Sequential()
+        model.add(Dense(num_hidden, input_dim=input_dim, activation='relu'))
+        model.add(Dense(num_classes, activation='softmax'))
+        model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+        cbks = [
+            LoggingModels(
+                filepath,
+                logger=logging.Logger(__name__),
+                monitor=monitor,
+                save_best_only=save_best_only,
+                mode=mode,
+            )
+        ]
+        model.fit(
+            X_train,
+            y_train,
+            batch_size=batch_size,
+            validation_data=(X_test, y_test),
+            callbacks=cbks,
+            epochs=1,
+        )
+        assert os.path.isfile(filepath)
+        os.remove(filepath)
+
+        # case 2
+        mode = 'min'
+        cbks = [
+            LoggingModels(
+                filepath,
+                logger=logging.Logger(__name__),
+                monitor=monitor,
+                save_best_only=save_best_only,
+                mode=mode,
+            )
+        ]
+        model.fit(
+            X_train,
+            y_train,
+            batch_size=batch_size,
+            validation_data=(X_test, y_test),
+            callbacks=cbks,
+            epochs=1,
+        )
+        assert os.path.isfile(filepath)
+        os.remove(filepath)
+
+        # case 3
+        mode = 'max'
+        monitor = 'val_acc'
+        cbks = [
+            LoggingModels(
+                filepath,
+                logger=logging.Logger(__name__),
+                monitor=monitor,
+                save_best_only=save_best_only,
+                mode=mode,
+            )
+        ]
+        model.fit(
+            X_train,
+            y_train,
+            batch_size=batch_size,
+            validation_data=(X_test, y_test),
+            callbacks=cbks,
+            epochs=1,
+        )
+        assert os.path.isfile(filepath)
+        os.remove(filepath)
+
+        # case 4
+        save_best_only = True
+        cbks = [
+            LoggingModels(
+                filepath,
+                logger=logging.Logger(__name__),
+                monitor=monitor,
+                save_best_only=save_best_only,
+                mode=mode,
+            )
+        ]
+        model.fit(
+            X_train,
+            y_train,
+            batch_size=batch_size,
+            validation_data=(X_test, y_test),
+            callbacks=cbks,
+            epochs=1,
+        )
+        assert os.path.isfile(filepath)
+        os.remove(filepath)
+
+        # case 5
+        monitor = 'val_loss'
+        save_best_only = False
+        mode = 'test'
+
+        model = Sequential()
+        model.add(Dense(num_hidden, input_dim=input_dim, activation='relu'))
+        model.add(Dense(num_classes, activation='softmax'))
+        model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+        cbks = [
+            LoggingModels(
+                filepath,
+                logger=logging.Logger(__name__),
+                monitor=monitor,
+                save_best_only=save_best_only,
+                mode=mode,
+            )
+        ]
+        model.fit(
+            X_train,
+            y_train,
+            batch_size=batch_size,
+            validation_data=(X_test, y_test),
+            callbacks=cbks,
+            epochs=1,
+        )
+        assert os.path.isfile(filepath)
+        mp_logger_warning.assert_called_once()
+        os.remove(filepath)
+
+        # case 6
+        save_best_only = False
+        period = 2
+        mode = 'auto'
+        filepath = 'checkpoint.{epoch:02d}.h5'
+        cbks = [
+            LoggingModels(
+                filepath,
+                logger=logging.Logger(__name__),
+                monitor=monitor,
+                save_best_only=save_best_only,
+                mode=mode,
+                period=period,
+            )
+        ]
+        model.fit(
+            X_train,
+            y_train,
+            batch_size=batch_size,
+            validation_data=(X_test, y_test),
+            callbacks=cbks,
+            epochs=4,
+        )
+        assert os.path.isfile(filepath.format(epoch=2))
+        assert os.path.isfile(filepath.format(epoch=4))
+        assert not os.path.exists(filepath.format(epoch=1))
+        assert not os.path.exists(filepath.format(epoch=3))
+        os.remove(filepath.format(epoch=2))
+        os.remove(filepath.format(epoch=4))
+        assert not os.listdir(tmpdir)
